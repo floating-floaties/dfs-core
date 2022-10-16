b966e4a742e21bef9e4475df8d3e8d9dea9276d9
Dustin <hunterghost526@gmail.com>
2022-10-16 05:50:11 -0500

    update

--- start.sh
+++ start.sh
@@ -17,11 +17,6 @@ if screen -list | grep -q "dfs-core"; then
     screen -S dfs-core -X quit
 fi
 
-export HOST=127.0.0.1
-export PORT=3000
-export ENV=dev
-export SAVE_LOGS=false
-
 # Start server on screen
 screen -dmS dfs-core
-screen -S dfs-core -X stuff "$HOME/dfs-core/bin/x86_64-unknown-linux-gnu/dfs\n"
+screen -S dfs-core -X stuff "./bin/x86_64-unknown-linux-gnu/dfs\n"
