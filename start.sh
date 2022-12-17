89a07250d47f506635105500bba439226afc02fb
Dustin <36748123+DustinDiazLopez@users.noreply.github.com>
2022-12-17 13:25:52 -0600

    update

--- start.sh
+++ start.sh
@@ -20,6 +20,11 @@ fi
 export LIBTORCH="$HOME/libtorch/"
 export LD_LIBRARY_PATH="$LIBTORCH/lib:$LD_LIBRARY_PATH"
 
+for i in $(lsof -t -i tcp:"8080"); do kill -9 "$i"; done
+for i in $(lsof -t -i tcp:"80"); do kill -9 "$i"; done
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
 # Start server on screen
 screen -dmS dfs-core
 screen -S dfs-core -X stuff "export LIBTORCH=\"$LIBTORCH\" \
@@ -29,7 +34,7 @@ screen -S dfs-core -X stuff "export LIBTORCH=\"$LIBTORCH\" \
                              && export CONFIG_CONCORD_APP_NAME=\"$CONFIG_CONCORD_APP_NAME\" \
                              && export CONFIG_CONCORD_EMAIL=\"$CONFIG_CONCORD_EMAIL\" \
                              && export HOST=\"0.0.0.0\" \
-                             && export PORT=\"80\" \
+                             && export PORT=\"8080\" \
                              && export ENV=\"production\" \
                              && export SAVE_LOGS=\"true\" \
                              && ./bin/x86_64-unknown-linux-gnu/dfs\n"
